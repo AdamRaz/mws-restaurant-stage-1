3927e1a9ac3b062c712d708d8836905dceca0bae
Adam Razzak <a.abdel@hotmail.co.uk>
2018-07-18 01:49:31 +0100

    feat: mapbox for inner pages

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'pk.eyJ1IjoiYWRhcmF6IiwiYSI6ImNqanBudnY3YzBxczczcm51Z294OHl4MnEifQ.SXQ018P8ZXRnw0dgM09NlA',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
